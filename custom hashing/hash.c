a92da54965b472269bde5f28e2c2a1b4439cdadc
Duelly01 <charles_duelas@dlsu.edu.ph>
2024-11-20 17:56:58 +0800

    added comments to the hashing function

--- custom hashing/hash.c
+++ custom hashing/hash.c
@@ -1,12 +1,25 @@
 #include "hash.h"
 
-// Custom hash function adapted to use table_size as a parameter
+/*  Custom Hash Function
+ *  0x7FFFFFFF
+    - djb2 and FNV hash were used for inspiration
+    - uses a prime multiplier (prime_multiplier)
+ */
 unsigned int custom_hash(const char *input, unsigned int table_size) {
-    unsigned int hash_value = 0;
+    unsigned int hash_value = 0; //will hold the computed hash during the loop
     unsigned int prime_multiplier = 37;
 
     int i;
+    /*  the loop will continue until it reaches the end of the string OR
+        the MAX_STRING_LENGTH is met
+    */ 
     for (i = 0; input[i] != '\0' && i < MAX_STRING_LENGTH; i++) {
+        /*  A. Each character's ASCII valued is mmultiplied by its position in the string
+                - To introduce positional information into the hash
+
+            B. The current hash_value is multiplied by the prime_multiplier (37)
+                - Spreads the bits to make sure 
+        */
         hash_value = (hash_value * prime_multiplier + input[i] * (i + 1)) & 0x7FFFFFFF;
     }
     return hash_value % table_size; // Use table_size to ensure hash fits
